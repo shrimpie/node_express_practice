2b5ec6734292aa7489bf332fd4a216b85eb1a9d5
杨振宇 <andy@young.local>
2016-12-21 15:15:41 -0500

    Try out some route examples

--- app/app.js
+++ app/app.js
@@ -4,48 +4,16 @@ var app = express();
 
 // You can load data like this.
 var dataFile = require('./data/data.json');
+app.set('appData', dataFile);
 
 // You can specify port too.
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res) {
-
-	var info = '';
-	dataFile.speakers.forEach(function(item) {
-		info += `
-		<li>
-		  <h2>${item.name}</h2>
-		  <p>${item.summary}</p>
-		</li>
-		`;
-	});
-
-	res.send(`
-		<h1>Roux Academy Meetups</h1>
-		${info}
-	`);
-
-});
+app.use(require('./routes/index'));
+app.use(require('./routes/speakers'));
 
 var server = app.listen(app.get('port'), function() {
 	console.log("Listening on port " + app.get('port'));
 });
-
-
-// // node uses require to load modules.
-// var http = require("http");
-
-// // the primitive way to create server.
-// var myServer = http.createServer(function(req, res) {
-
-// 	res.writeHead(200, {"Content-Type" : "text/html"});
-
-// 	res.write("<h1>Roux Meetups</h1>");
-
-// 	res.end();
-// });
-
-// myServer.listen(3000);
-// console.log("Server listening at localhost on port 3000");
 
 
