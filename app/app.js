fc9bb66ad60f6445b2b718082da31a0186038892
杨振宇 <andy@young.local>
2016-12-21 15:43:38 -0500

    Use nodemon and reload to automate workflow

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var reload = require('reload');
 
 var app = express();
+
 
 // You can load data like this.
 var dataFile = require('./data/data.json');
@@ -18,4 +20,4 @@ var server = app.listen(app.get('port'), function() {
 	console.log("Listening on port " + app.get('port'));
 });
 
-
+reload(server, app);
