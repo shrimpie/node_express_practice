97fbac839e5ea3c94095891f77115ec5f684764d
杨振宇 <andy@young.local>
2016-12-22 11:52:18 -0500

    add feedback functionality

--- app/routes/feedback.js
+++ app/routes/feedback.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/feedback', function(req, res) {
 
-	res.render('feedback',{
+	res.render('feedback', {
 		pageTitle: 'Feedback',
 		pageID: 'feedback'
 	});
